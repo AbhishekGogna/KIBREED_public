cfd44dd1904e1cd893e2d5190c4af3e1c2b0e683
Abhishek Gogna <gogna@slurm-01.ipk-gatersleben.de>
2025-06-13 13:59:15 +0200

    revised summaries for cnn

--- Py/model_CNN_EV.py
+++ Py/model_CNN_EV.py
@@ -3,24 +3,23 @@ from .func import CNN_net_fixed, CNN_net_flex, dense_post_concat
 
 # Model 3 ---------------------------------------------------------------------------------------------------------------------
 class tuner(kt.HyperModel):
-    def __init__(self, marker_n, ec_n, tune = False):
+    def __init__(self, marker_n, ev_n, tune = False):
         self.marker_n = marker_n # markers
-        self.ec_n = ec_n
+        self.ev_n = ev_n
         self.tune = tune
-        print(f'markers = {self.marker_n} and env variables = {self.ec_n}')
+        print(f'markers = {self.marker_n} and env variables = {self.ev_n}')
 
     def build(self, hp):
-        # generate network for ec data
-        net_g_s = CNN_net_fixed(hp, n_in = self.ec_n, 
-                               model_name = "g_s", 
+        # generate network for ev data
+        net_ev = CNN_net_fixed(hp, n_in = self.ev_n, 
+                               model_name = "ev", 
                                kernel_size = 2)
         # generate network for g_a data
         net_g_a = CNN_net_flex(hp, n_in = self.marker_n, 
                                model_name = "g_a", 
                                base_layer = 7, max_layer = 3, tuning = self.tune)
-        #net_g_d = CNN_g_data(hp, self.marker_n, "g_d")
         X_list = [net_g_a]
-        X_list.append(net_g_s) # should follow the order of inputs provided
+        X_list.append(net_ev) # should follow the order of inputs provided
         
         # split list for input and output
         input_list = [x[0] for x in X_list]
