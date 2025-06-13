799a9876251e30e7674899e49bce2b4e633e66f5
Abhishek Gogna <gogna@slurm-01.ipk-gatersleben.de>
2025-06-13 13:50:24 +0200

    revised summaries for cnn

--- Py/model_CNN_EV.py
+++ Py/model_CNN_EV.py
@@ -3,12 +3,11 @@ from .func import CNN_net_fixed, CNN_net_flex, dense_post_concat
 
 # Model 3 ---------------------------------------------------------------------------------------------------------------------
 class tuner(kt.HyperModel):
-    def __init__(self, marker_n, ec_n, tune = False, batch_norm = False):
+    def __init__(self, marker_n, ec_n, tune = False):
         self.marker_n = marker_n # markers
         self.ec_n = ec_n
         self.tune = tune
-        self.batch_norm = batch_norm
-        print(f'markers = {self.marker_n} and g_a_s\'s = {self.ec_n}')
+        print(f'markers = {self.marker_n} and env variables = {self.ec_n}')
 
     def build(self, hp):
         # generate network for ec data
@@ -29,10 +28,6 @@ class tuner(kt.HyperModel):
         
         # concat inputs
         model_concat = concatenate(output_list, name = "concat_in")
-        
-        if self.batch_norm:
-            # do normalization
-            model_concat = BatchNormalization(name = "norm_in")(model_concat) # i don't know enoough about this to be confident in using it
         
         model_compiled = dense_post_concat(hp, input_list, model_concat, tuning = not self.tune)
         
