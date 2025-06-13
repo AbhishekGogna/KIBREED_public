2b83c3417530b0e1683ab74c924e9954f8db4a11
Abhishek Gogna <gogna@slurm-01.ipk-gatersleben.de>
2025-06-13 09:03:28 +0200

    added additional model to wtn

--- Py/func.py
+++ Py/func.py
@@ -136,3 +136,46 @@ def CNN_net_flex(hp, model_name, base_layer, max_layer, n_in = None, ec_layer = 
     # flatten last layer
     CNN_output = Flatten(name=f'CNN_{model_name}_flat')(CNN)
     return model_input, CNN_output
+
+def fit_model(final_model, params, train_x, val_x, train_y, val_y):
+     
+    # set variables
+    tb_filepath, cp_filepath, b_size, epoch, vbs, sfl = [params['fit'][key] for key in ['tensorboard_fp', 'checkpoint_fp', 'batch_size', 'epochs', 'verbose', 'shuffle']]
+    
+    #set call backs
+    tensorboard_cb = TensorBoard(tb_filepath)
+    modelcheck_cb = ModelCheckpoint(filepath=cp_filepath,
+                                    save_weights_only=True,
+                                    monitor='val_loss',
+                                    mode='min',
+                                    save_best_only=True)
+    model_cb = EarlyStopping(monitor='val_loss',
+                                     min_delta=0.00001,
+                                     patience=5,
+                                     verbose=0,
+                                     mode='min',
+                                     baseline=None,
+                                     restore_best_weights=True)
+    final_model.fit(train_x, train_y, validation_data=(val_x, val_y),
+                    batch_size = b_size,
+                    epochs = epoch,
+                    verbose = vbs,
+                    shuffle = sfl,
+                    callbacks=[modelcheck_cb, 
+                               tensorboard_cb,
+                               model_cb])
+    
+    final_model.load_weights(cp_filepath) # loads best weights
+    return final_model
+
+def predict_values (model, test_x, test_y, index, scaler):
+    
+    # perform predictions
+    prediction = model.predict(test_x)
+    
+    # re-scale data
+    obs = inverse_scale(scaler, test_y, verbose = False)
+    pred = inverse_scale(scaler, prediction, verbose = False)
+    out_data = pd.DataFrame([index, obs, pred], index=["index","obs","pred"]).T
+    out_data["index"] = out_data["index"].astype('int')
+    return out_data
